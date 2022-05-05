ed645eb5eb446db5b14ae4a5833db1df0a941f1f
crimsonwoods <crimsonwoods.dev@gmail.com>
2022-05-05 14:01:22 +0900

    :shirt: Adds new tests for value only JSON

--- lib/src/test/java/net/crimsonwoods/retrofit/converter/moshi/MoshiFormRequestBodyConverterTest.kt
+++ lib/src/test/java/net/crimsonwoods/retrofit/converter/moshi/MoshiFormRequestBodyConverterTest.kt
@@ -29,4 +29,108 @@ class MoshiFormRequestBodyConverterTest {
         assertEquals("[]", actual.name(1))
         assertEquals("abcdefg", actual.value(1))
     }
+
+    @Test
+    fun valueOnly() {
+
+        val inputs = listOf<ValueOnlyInput<*>>(
+            ValueOnlyInput.S("test"),
+            ValueOnlyInput.N(123L),
+            ValueOnlyInput.B(true),
+            ValueOnlyInput.Null,
+        )
+
+        inputs.forEach { input ->
+            val (actual, expected) = when (input) {
+                is ValueOnlyInput.S -> {
+                    val converter = MoshiFormRequestBodyConverter(
+                        adapter = moshi.adapter(input.type),
+                        hasArrayIndex = false,
+                    )
+
+                    converter.convert(input.value) to input.expected
+                }
+                is ValueOnlyInput.N -> {
+                    val converter = MoshiFormRequestBodyConverter(
+                        adapter = moshi.adapter(input.type),
+                        hasArrayIndex = false,
+                    )
+
+                    converter.convert(input.value) to input.expected
+                }
+                is ValueOnlyInput.B -> {
+                    val converter = MoshiFormRequestBodyConverter(
+                        adapter = moshi.adapter(input.type),
+                        hasArrayIndex = false,
+                    )
+
+                    converter.convert(input.value) to input.expected
+                }
+                is ValueOnlyInput.Null -> {
+                    val converter = MoshiFormRequestBodyConverter(
+                        adapter = moshi.adapter(input.type).nullSafe(),
+                        hasArrayIndex = false,
+                    )
+
+                    converter.convert(input.value) to input.expected
+                }
+            }
+
+            assertNotNull(actual)
+            assertTrue(actual is FormBody)
+            assertEquals(1, actual.size)
+            assertEquals("", actual.name(0))
+            assertEquals(expected, actual.value(0))
+        }
+    }
+
+    sealed interface ValueOnlyInput<T : Any> {
+        val type: Class<T>
+        val value: T?
+        val expected: String
+
+        data class S(private val v: String) : ValueOnlyInput<String> {
+            override val type: Class<String>
+                get() = String::class.java
+
+            override val value: String
+                get() = v
+
+            override val expected: String
+                get() = v
+        }
+
+        data class N(private val v: Long) : ValueOnlyInput<Long> {
+            override val type: Class<Long>
+                get() = Long::class.java
+
+            override val value: Long
+                get() = v
+
+            override val expected: String
+                get() = v.toString()
+        }
+
+        data class B(private val v: Boolean) : ValueOnlyInput<Boolean> {
+            override val type: Class<Boolean>
+                get() = Boolean::class.java
+
+            override val value: Boolean
+                get() = v
+
+            override val expected: String
+                get() = v.toString()
+        }
+
+        object Null : ValueOnlyInput<String> {
+            override val type: Class<String>
+                get() = String::class.java
+
+            override val value: String?
+                get() = null
+
+            override val expected: String
+                get() = "null"
+        }
+    }
 }
