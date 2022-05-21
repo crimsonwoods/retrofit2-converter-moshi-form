a9c659ee4a8f4e10fa5af5e743698980ef71eaf9
crimsonwoods <crimsonwoods.dev@gmail.com>
2022-05-21 14:58:28 +0900

    :shirt: Adds more tests for MoshiFormRequestBodyConverter

--- lib/src/test/java/net/crimsonwoods/retrofit/converter/moshi/MoshiFormRequestBodyConverterTest.kt
+++ lib/src/test/java/net/crimsonwoods/retrofit/converter/moshi/MoshiFormRequestBodyConverterTest.kt
@@ -1,6 +1,7 @@
 package net.crimsonwoods.retrofit.converter.moshi
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import okhttp3.FormBody
 import org.junit.Test
@@ -12,6 +13,91 @@ import kotlin.test.assertTrue
 @RunWith(AndroidJUnit4::class)
 class MoshiFormRequestBodyConverterTest {
     private val moshi = Moshi.Builder().build()
+
+    @JsonClass(generateAdapter = true)
+    data class InputDouble(
+        val value: Double,
+        val array: List<Double>,
+    )
+
+    @JsonClass(generateAdapter = true)
+    data class InputLong(
+        val value: Long,
+        val array: List<Long>,
+    )
+
+    @Test
+    fun double() {
+        val converter = MoshiFormRequestBodyConverter(
+            adapter = moshi.adapter(InputDouble::class.java),
+            hasArrayIndex = false,
+        )
+
+        data class Input(
+            val input: InputDouble,
+            val expected: List<Pair<String, String>>,
+        )
+
+        val inputs = listOf(
+            Input(
+                input = InputDouble(0.1, listOf(0.2)),
+                expected = listOf("value" to "0.1", "array[]" to "0.2")
+            ),
+            Input(
+                input = InputDouble(Double.MAX_VALUE, listOf(Double.MIN_VALUE)),
+                expected = listOf(
+                    "value" to "${Double.MAX_VALUE}",
+                    "array[]" to "${Double.MIN_VALUE}"
+                )
+            ),
+        )
+
+        inputs.forEach { input ->
+            val actual = converter.convert(input.input)
+            assertNotNull(actual)
+            assertTrue(actual is FormBody)
+            assertEquals(input.expected.size, actual.size)
+            assertEquals(
+                input.expected,
+                (0 until actual.size).map { index -> actual.name(index) to actual.value(index) }
+            )
+        }
+    }
+
+    @Test
+    fun long() {
+        val converter = MoshiFormRequestBodyConverter(
+            adapter = moshi.adapter(InputLong::class.java),
+            hasArrayIndex = false,
+        )
+
+        data class Input(
+            val input: InputLong,
+            val expected: List<Pair<String, String>>,
+        )
+
+        val inputs = listOf(
+            Input(
+                input = InputLong(1L, listOf(2L)),
+                expected = listOf("value" to "1", "array[]" to "2")
+            ),
+            Input(
+                input = InputLong(Long.MAX_VALUE, listOf(Long.MIN_VALUE)),
+                expected = listOf("value" to "${Long.MAX_VALUE}", "array[]" to "${Long.MIN_VALUE}")
+            ),
+        )
+
+        inputs.forEach { input ->
+            val actual = converter.convert(input.input)
+            assertNotNull(actual)
+            assertTrue(actual is FormBody)
+            assertEquals(input.expected.size, actual.size)
+            assertEquals(
+                input.expected,
+                (0 until actual.size).map { index -> actual.name(index) to actual.value(index) }
+            )
+        }
+    }
 
     @Test
     fun arrayOnly() {
